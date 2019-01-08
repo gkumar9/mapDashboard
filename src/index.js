a7dca88df166a3f2488546672d5a38f0f9c02704
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2019-01-08 22:01:15 +0530

    code for build in base ref

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createBrowserHistory } from 'history';
 // import App from './App';
 // import SiteMapGoogle from './SiteMapGoogle.js';
 // import Maps from './Maps.js'
 import UI from	'./UI.js'
 // import Reactmaps from './ReactMap.js'
 import * as serviceWorker from './serviceWorker';
+
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
 
 ReactDOM.render(<UI />, document.getElementById('root'));
 
