22dbc31706dbe73652b66a217e03bf1c804ec969
gkumar9 <gaurav9904@gmail.com>
2019-05-17 10:23:08 +0530

    config change

--- src/config.js
+++ src/config.js
@@ -1,72 +1,72 @@
-// let config={
-// 	"allpins":"../api/rs/claro/maps/all/pins",
-// 	"minigrid":"../api/rs/claro/maps/asset/info/minigrid",
-// 	"patvan":"../api/rs/claro/maps/asset/info/patvan",
-// 	"rooftop":"../api/rs/claro/maps/asset/info/rooftop",
-// 	"irrigation":"../api/rs/claro/maps/asset/info/pump/irrigation",
-// 	"drinkingwater":"../api/rs/claro/maps/asset/info/pump/drinkingwater",
-// 	"LogoutServlet":"../LogoutServlet",
-// 	"rmslist":"../rms/api/rs/assets/list",
-// 	"highchartdata":"../rms/api/rs/asset/highchart/data",
-// 	"allassetstat":"../rms/api/rs/all/assets/stats",
-// 	"singleassetstat":"../rms/api/rs/asset/stats",
-//  	"fvcstat":"../rms/api/rs/asset/activity",
-// 	"hourtrans":"../iaas/api/rs/highchart/transaction/byduration",
-// 	"revenue":"../iaas/api/rs/highchart/revenue/bytimeline",
-// 	"iaaspatvan":"../iaas/api/rs/highchart/revenue/bypatvan",
-// 	"iaasrevenueyearly":"../iaas/api/rs/highchart/revenue/yearly",
-// 	"iaasrevenuemonthly":"../iaas/api/rs/highchart/revenue/monthly/",
-// 	"iaasrevenuedaily":"../iaas/api/rs/highchart/revenue/daily/",
-// 	"iaasstats":"../iaas/api/rs/widget/home",
-// 	"transactionlist":"../iaas/api/rs/transactionlist",
-// 	"farmerstate":"../farmerinfo/farmerstats/india",
-// 	"farmerdistrict":"../farmerinfo/farmerstats/india/",
-// 	"farmertable":'../farmerinfo/farmerstats/india/',
-// "farmerinfo":"../farmerinfo/farmerinfo",
-// "farmercoordinates":"../farmerinfo/farmercoordinates"
-// }
+let config={
+	"allpins":"../api/rs/claro/maps/all/pins",
+	"minigrid":"../api/rs/claro/maps/asset/info/minigrid",
+	"patvan":"../api/rs/claro/maps/asset/info/patvan",
+	"rooftop":"../api/rs/claro/maps/asset/info/rooftop",
+	"irrigation":"../api/rs/claro/maps/asset/info/pump/irrigation",
+	"drinkingwater":"../api/rs/claro/maps/asset/info/pump/drinkingwater",
+	"LogoutServlet":"../LogoutServlet",
+	"rmslist":"../rms/api/rs/assets/list",
+	"highchartdata":"../rms/api/rs/asset/highchart/data",
+	"allassetstat":"../rms/api/rs/all/assets/stats",
+	"singleassetstat":"../rms/api/rs/asset/stats",
+ 	"fvcstat":"../rms/api/rs/asset/activity",
+	"hourtrans":"../iaas/api/rs/highchart/transaction/byduration",
+	"revenue":"../iaas/api/rs/highchart/revenue/bytimeline",
+	"iaaspatvan":"../iaas/api/rs/highchart/revenue/bypatvan",
+	"iaasrevenueyearly":"../iaas/api/rs/highchart/revenue/yearly",
+	"iaasrevenuemonthly":"../iaas/api/rs/highchart/revenue/monthly/",
+	"iaasrevenuedaily":"../iaas/api/rs/highchart/revenue/daily/",
+	"iaasstats":"../iaas/api/rs/widget/home",
+	"transactionlist":"../iaas/api/rs/transactionlist",
+	"farmerstate":"../farmerinfo/farmerstats/india",
+	"farmerdistrict":"../farmerinfo/farmerstats/india/",
+	"farmertable":'../farmerinfo/farmerstats/india/',
+"farmerinfo":"../farmerinfo/farmerinfo",
+"farmercoordinates":"../farmerinfo/farmercoordinates"
+}
 
-let config = {
-  allpins: "http://staging.clarolabs.in:7060/api/rs/claro/maps/all/pins",
-  minigrid:
-    "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/minigrid",
-  patvan:
-    "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/patvan",
-  rooftop:
-    "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/rooftop",
-  irrigation:
-    "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/pump/irrigation",
-  drinkingwater:
-    "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/pump/drinkingwater",
-  LogoutServlet: "http://staging.clarolabs.in:7060/LogoutServlet",
-  rmslist: "http://staging.clarolabs.in:7060/rms/api/rs/assets/list",
-  highchartdata:
-    "http://staging.clarolabs.in:7060/rms/api/rs/asset/highchart/data",
-  allassetstat: "http://staging.clarolabs.in:7060/rms/api/rs/all/assets/stats",
-  singleassetstat: "http://staging.clarolabs.in:7060/rms/api/rs/asset/stats",
-  fvcstat: "http://staging.clarolabs.in:7060/rms/api/rs/asset/activity",
-  hourtrans:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/transaction/byduration",
-  revenue:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/bytimeline",
-  iaaspatvan:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/bypatvan",
-  iaasrevenueyearly:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/yearly",
-  iaasrevenuemonthly:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/monthly/",
-  iaasrevenuedaily:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/daily/",
-  iaasstats: "http://staging.clarolabs.in:7060/iaas/api/rs/widget/home",
-  transactionlist:
-    "http://staging.clarolabs.in:7060/iaas/api/rs/transactionlist",
-  farmerstate: "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india",
-  farmerdistrict:
-    "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india/",
-  farmertable: "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india/",
-  farmerinfo: "http://staging.clarolabs.in:7060/farmerinfo/farmerinfo",
-  farmercoordinates:
-    "http://staging.clarolabs.in:7060/farmerinfo/farmercoordinates"
-};
+// let config = {
+//   allpins: "http://staging.clarolabs.in:7060/api/rs/claro/maps/all/pins",
+//   minigrid:
+//     "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/minigrid",
+//   patvan:
+//     "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/patvan",
+//   rooftop:
+//     "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/rooftop",
+//   irrigation:
+//     "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/pump/irrigation",
+//   drinkingwater:
+//     "http://staging.clarolabs.in:7060/api/rs/claro/maps/asset/info/pump/drinkingwater",
+//   LogoutServlet: "http://staging.clarolabs.in:7060/LogoutServlet",
+//   rmslist: "http://staging.clarolabs.in:7060/rms/api/rs/assets/list",
+//   highchartdata:
+//     "http://staging.clarolabs.in:7060/rms/api/rs/asset/highchart/data",
+//   allassetstat: "http://staging.clarolabs.in:7060/rms/api/rs/all/assets/stats",
+//   singleassetstat: "http://staging.clarolabs.in:7060/rms/api/rs/asset/stats",
+//   fvcstat: "http://staging.clarolabs.in:7060/rms/api/rs/asset/activity",
+//   hourtrans:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/transaction/byduration",
+//   revenue:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/bytimeline",
+//   iaaspatvan:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/bypatvan",
+//   iaasrevenueyearly:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/yearly",
+//   iaasrevenuemonthly:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/monthly/",
+//   iaasrevenuedaily:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/highchart/revenue/daily/",
+//   iaasstats: "http://staging.clarolabs.in:7060/iaas/api/rs/widget/home",
+//   transactionlist:
+//     "http://staging.clarolabs.in:7060/iaas/api/rs/transactionlist",
+//   farmerstate: "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india",
+//   farmerdistrict:
+//     "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india/",
+//   farmertable: "http://staging.clarolabs.in:7060/farmerinfo/farmerstats/india/",
+//   farmerinfo: "http://staging.clarolabs.in:7060/farmerinfo/farmerinfo",
+//   farmercoordinates:
+//     "http://staging.clarolabs.in:7060/farmerinfo/farmercoordinates"
+// };
 
 export default config;
